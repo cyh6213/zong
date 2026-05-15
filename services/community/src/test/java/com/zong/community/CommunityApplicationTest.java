0094dba0dba2b2a7ad439f6dd088ca3953e904e0
cyh <2952486829@qq.com>
2026-05-15 18:43:30 +0800

    feat: create community service skeleton

---
+++ services/community/src/test/java/com/zong/community/CommunityApplicationTest.java
@@ -0,0 +1,11 @@
+package com.zong.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
