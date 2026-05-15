32fb78be26f6d945c3cbacb0b7c97fcd69e8d1a3
cyh <2952486829@qq.com>
2026-05-15 18:43:00 +0800

    feat: create shared module with Result, BusinessException, GlobalExceptionHandler, ServiceConstants

---
+++ shared/src/main/java/com/zong/shared/constants/ServiceConstants.java
@@ -0,0 +1,9 @@
+package com.zong.shared.constants;
+
+public final class ServiceConstants {
+    private ServiceConstants() {}
+
+    public static final String COMMUNITY_SERVICE = "community";
+    public static final String KNOWLEDGE_SERVICE = "knowledge";
+    public static final String AGENT_SERVICE = "agent";
+}
---
+++ shared/src/main/java/com/zong/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.zong.shared.exception;
+
+import com.zong.shared.response.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public Result<Void> handleBusinessException(BusinessException e) {
+        return Result.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception e) {
+        return Result.error(500, "Internal server error");
+    }
+}
