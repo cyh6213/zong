9954dd8ec056f5aba4a92d70eaf302dd2536c8e4
cyh <2952486829@qq.com>
2026-05-15 18:43:46 +0800

    feat: create knowledge service skeleton with RAG support

---
+++ services/knowledge/src/test/java/com/zong/knowledge/KnowledgeApplicationTest.java
@@ -0,0 +1,11 @@
+package com.zong.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
